31009108a760462a8113918819c23336da0c4451
Emre Surmeli <s.emre.s.8@gmail.com>
2019-07-12 18:33:38 -0400

    Update migrations to call multisig with a few accounts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,10 +2,15 @@ const ProofOfExistence1 = artifacts.require('../contracts/ProofOfExistence1.sol'
 const ProofOfExistence2 = artifacts.require('../contracts/ProofOfExistence2.sol');
 const ProofOfExistence3 = artifacts.require('../contracts/ProofOfExistence3.sol');
 const MultiSignatureWallet = artifacts.require('../contracts/MultiSignatureWallet.sol');
+const SimpleStorage = artifacts.require('../contracts/SimpleStorage.sol');
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network, accounts) {
+  const owners = [accounts[0], accounts[1]]
+
   deployer.deploy(ProofOfExistence1);
   deployer.deploy(ProofOfExistence2);
   deployer.deploy(ProofOfExistence3);
-  deployer.deploy(MultiSignatureWallet);
+
+  deployer.deploy(MultiSignatureWallet, owners, 2);
+  deployer.deploy(SimpleStorage);
 };
